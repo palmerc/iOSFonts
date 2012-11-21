39297b56c0a620d6a9794350392ba868ff68a32d
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2012-11-21 13:22:30 +0100

    Finished a quick and dirty app to display the fonts.

--- iOS5Fonts/CPViewController.m
+++ iOS5Fonts/CPViewController.m
@@ -14,14 +14,38 @@
 
 @implementation CPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSString *quickFox = @"The quick brown fox jumped over the lazy dog.";
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:scrollView];
+
+    
+    NSArray *familyNames = [[UIFont familyNames] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    
+    CGFloat y = 0.0f;
+    for (NSString *fontName in familyNames) {
+        NSString *displayText = [NSString stringWithFormat:@"%@: %@", fontName, quickFox];
+        
+        UIFont *font = [UIFont fontWithName:fontName size:24.0f];
+        CGSize size = [displayText sizeWithFont:font];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, size.width, size.height)];
+        label.font = font;
+        label.text = displayText;
+        y += size.height;
+        
+        [scrollView addSubview:label];
+        [label release];
+    }
+    
+    scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, y);
+    
+    [scrollView release];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
