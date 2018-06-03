6120befad319a71ba55d4ea04bee007ebadad4dc
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2018-06-03 16:03:30 +0200

    Update for Xcode SDK 11

--- iOS5Fonts/CPViewController.m
+++ iOS5Fonts/CPViewController.m
@@ -12,55 +12,67 @@
     NSString *quickFox = @"The quick brown fox jumped over the lazy dog.";
     
     UIScrollView *scrollView = [[UIScrollView alloc] init];
-    [self.view addSubview:scrollView];
     scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:scrollView];
 
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(scrollView);
-    NSArray *horizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollView]|" options:0 metrics:nil views:viewsDictionary];
-    [self.view addConstraints:horizontalConstraints];
-    NSArray *verticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[scrollView]-|" options:0 metrics:nil views:viewsDictionary];
-    [self.view addConstraints:verticalConstraints];
-    
-    NSArray *familyNames = [[UIFont familyNames] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    UIFont *fontNameFont = [UIFont fontWithName:@"Courier" size:14.0];
+    NSArray<NSString *> *familyNames = [[UIFont familyNames] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
 
-    UIView *previousView = nil;
+    NSMutableArray *labels = [[NSMutableArray alloc] initWithCapacity:[familyNames count]];
     for (NSString *fontName in familyNames) {
+        NSString *baseString = [fontName stringByAppendingString:@": "];
+        
+        NSMutableAttributedString *fontNameAttributedString = [[NSMutableAttributedString alloc] initWithString:baseString];
+        [fontNameAttributedString addAttribute:NSFontAttributeName value:fontNameFont range:NSMakeRange(0, baseString.length)];
+        
+        UIFont *demoFont = [UIFont fontWithName:fontName size:24.0f];
+        NSMutableAttributedString *demoFontAttributedString = [[NSMutableAttributedString alloc] initWithString:quickFox];
+        [demoFontAttributedString addAttribute:NSFontAttributeName value:demoFont range:NSMakeRange(0, quickFox.length)];
+    
+        [fontNameAttributedString appendAttributedString:demoFontAttributedString];
+        
+        NSAttributedString *attributedText = fontNameAttributedString;
+        
         UILabel *label = [[UILabel alloc] init];
         label.translatesAutoresizingMaskIntoConstraints = NO;
-        [label setAdjustsFontSizeToFitWidth:YES];
-        label.font = [UIFont fontWithName:fontName size:24.0f];
-        label.text = [NSString stringWithFormat:@"%@: %@", fontName, quickFox];
+        label.attributedText = attributedText;
 
         [scrollView addSubview:label];
-
-        NSDictionary *views = NSDictionaryOfVariableBindings(label);
-        [scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[label]" options:0 metrics:nil views:views]];
         
-        if (previousView == nil) {
-            [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:scrollView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
-        } else {
-             [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:previousView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:8.0]];
-        }
-        
-        previousView = label;
+        [labels addObject:label];
     }
     
-    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:scrollView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:previousView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:scrollView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:scrollView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeTopMargin relatedBy:NSLayoutRelationEqual toItem:scrollView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:scrollView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottomMargin multiplier:1.0 constant:0.0]];
+    
+    UILabel *previousLabel = nil;
+    for (UILabel *label in labels) {
+        // Label left-side horizontal
+        [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:scrollView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:8.0]];
+        
+        // Label right-side horizontal
+        [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:scrollView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:label attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:8.0]];
+        
+        // Label top
+        if (previousLabel == nil) {
+            [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:scrollView attribute:NSLayoutAttributeTop multiplier:1.0 constant:8.0]];
+        } else {
+            // Label top of label to bottom of previous
+            [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:previousLabel attribute:NSLayoutAttributeBottom multiplier:1.0 constant:8.0]];
+        }
+        
+        previousLabel = label;
+    }
+    
+    // Last label attached to bottom of scrollview
+    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:scrollView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:previousLabel attribute:NSLayoutAttributeBottom multiplier:1.0 constant:8.0]];
     
     self.scrollView = scrollView;
-}
-
-- (void)viewDidLayoutSubviews {
-    for (UIView *view in self.scrollView.subviews) {
-        if ([view isKindOfClass:[UILabel class]]) {
-            UILabel *label = (UILabel *)view;
-            [label setNeedsLayout];
-            [label sizeToFit];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                label.preferredMaxLayoutWidth = label.bounds.size.width;
-            });
-        }
-    }
 }
 
 - (void)didReceiveMemoryWarning {
